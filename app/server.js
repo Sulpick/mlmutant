9ca18b1bf3e1ffcea5663cdf87015faaeaddc318
ext_sbarbellag <ext_sbarbellag@tottus.cl>
2021-06-17 14:01:44 -0500

    misc

--- app/server.js
+++ app/server.js
@@ -3,12 +3,13 @@ const bodyParser = require('koa-bodyparser');
 const mutantRoute = require('./routes/mutant');
 const database = require('./utils/database');
 const { logger } = require('./services/logger');
+require('dotenv').config();
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(mutantRoute.routes());
-app.listen(8080, async () => {
+app.listen(process.env.PORT, async () => {
   await database.start();
-  logger.info('Servidor escuchando desde el puerto 8080');
+  logger.info(`Servidor escuchando desde el puerto ${process.env.PORT}`);
 });
